e8cf28a7d30e6c170391863dc9881ed603950d21
Techno <Techno@76c3dd84-03ae-47c5-963a-2991f67f3736>
2011-01-27 23:31:02 +0000

    Added exit codes for success / failure

--- src/org/ua2/apuawling/Server.java
+++ src/org/ua2/apuawling/Server.java
@@ -1,5 +1,5 @@
 /**
- * UA3 HTTP API server
+ * UA2 HTTP API server
  * 
  * Based on http://patriot.net/~tvalesky/TeenyWeb.java
  * 
@@ -20,7 +20,7 @@ import org.apache.log4j.Logger;
 public class Server implements Runnable {
 
 	public static final String CLIENT = "apUAwling";
-	public static final String VERSION = "0.6c";
+	public static final String VERSION = "0.6e";
 
 	private ServerSocket listener;
 	private ExecutorService service;
@@ -97,6 +97,10 @@ public class Server implements Runnable {
 			server.run();
 		} catch(Exception e) {
 			e.printStackTrace();
+			
+			System.exit(1);
 		}
+		
+		System.exit(0);
 	}
 }
