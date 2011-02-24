ef71035938442e1bae2eeb23b124efb357d517a8
Techno <Techno@76c3dd84-03ae-47c5-963a-2991f67f3736>
2011-02-24 23:44:19 +0000

    Added trailing whitespace test

--- test/org/ua2/apuawling/TestJSONWrapper.java
+++ test/org/ua2/apuawling/TestJSONWrapper.java
@@ -5,7 +5,6 @@ import junit.framework.TestCase;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.ua2.apuawling.JSONWrapper;
 
 public class TestJSONWrapper extends TestCase {
 	
@@ -15,8 +14,14 @@ public class TestJSONWrapper extends TestCase {
 		assertEquals("minge", ((JSONObject)obj1.getArray().get(1)).getString("folder"));
 		
 		JSONWrapper obj2 = JSONWrapper.parse("{ \"response\": [{\"folder\":\"badger\"},{\"folder\":\"minge\"}]}");
-		JSONArray array = (JSONArray)obj2.getObject().get("response");
-		assertEquals("badger", ((JSONObject)array.get(0)).getString("folder"));
-		assertEquals("minge", ((JSONObject)array.get(1)).getString("folder"));
+		JSONArray array2 = (JSONArray)obj2.getObject().get("response");
+		assertEquals("badger", ((JSONObject)array2.get(0)).getString("folder"));
+		assertEquals("minge", ((JSONObject)array2.get(1)).getString("folder"));
+		
+		JSONWrapper obj3 = JSONWrapper.parse("[1,2,3]   ");
+		JSONArray array3 = (JSONArray)obj3.getArray();
+		assertEquals(1, array3.getInt(0));
+		assertEquals(2, array3.getInt(1));
+		assertEquals(3, array3.getInt(2));
 	}
 }
