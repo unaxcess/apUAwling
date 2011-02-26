53560bec60e201d89fe9656d57e31ce85745a47b
Techno <Techno@76c3dd84-03ae-47c5-963a-2991f67f3736>
2011-02-26 22:16:07 +0000

    Add NDC logging, check for more than one forwarded header value

--- src/org/ua2/apuawling/Server.java
+++ src/org/ua2/apuawling/Server.java
@@ -20,7 +20,7 @@ import org.apache.log4j.Logger;
 public class Server implements Runnable {
 
 	public static final String CLIENT = "apUAwling";
-	public static final String VERSION = "0.7a";
+	public static final String VERSION = "0.7b";
 
 	private ServerSocket listener;
 	private ExecutorService service;
